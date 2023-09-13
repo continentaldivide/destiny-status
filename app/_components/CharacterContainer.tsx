fa2b27c1cc6a97abd01142dcdff9606df87e0aef
Andrew <andrew@andrewsmith.org>
2023-09-13 00:23:28 -0400

    Simplify relationship between CharacterContainer and Character

--- app/_components/CharacterContainer.tsx
+++ app/_components/CharacterContainer.tsx
@@ -2,27 +2,11 @@ import Character from './Character';
 import { usePlayerContext } from '../_context/PlayerContext';
 
 export default function CharacterContainer() {
-  const { characterEquipment, itemInstances } = usePlayerContext();
+  const { characterEquipment } = usePlayerContext();
   const characterIds = Object.keys(characterEquipment);
 
   const characters = characterIds.map((characterId, i) => {
-    const itemHashes = characterEquipment[characterId].items.map((item: any) => {
-      return item.itemHash;
-    });
-    const itemInstanceIds = characterEquipment[characterId].items.map(
-      (item: any) => {
-        return item.itemInstanceId;
-      }
-    );
-
-    return (
-      <Character
-        itemHashes={itemHashes}
-        itemInstanceIds={itemInstanceIds}
-        itemInstances={itemInstances}
-        key={i}
-      />
-    );
+    return <Character characterId={characterId} key={i} />;
   });
 
   return <div className="flex gap-4">{characters}</div>;
