79de2936fd7eb4c0546cf904a16a650b71a9d542
Andrew <andrew@andrewsmith.org>
2023-09-21 18:30:38 -0400

    Use form for nav search

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -1,19 +1,28 @@
 import { useState } from 'react';
+import Image from 'next/image';
 
 export default function Nav({ setUsername }) {
   const [searchQuery, setSearchQuery] = useState('');
   return (
-    <nav className="absolute w-full bg-slate-700 border-b-2 border-slate-100">
-      <div className="flex justify-center">
+    <nav className="absolute w-full flex justify-center bg-slate-700 border-b-2 border-slate-100">
+      <form action="" className="flex w-1/3">
         <input
           value={searchQuery}
           type="search"
-          placeholder="hello"
+          placeholder="search for a destiny player"
           onChange={(e) => setSearchQuery(e.target.value)}
-          className="h-10 w-1/3 text-2xl outline-none bg-green-200"
+          className="h-10 flex-grow text-2xl outline-none bg-green-200"
         />
-        <button onClick={() => setUsername(searchQuery)}>button</button>
-      </div>
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            setUsername(searchQuery);
+          }}
+        >
+          <Image src="search.svg" alt="" width={40} height={40} />
+        </button>
+      </form>
     </nav>
   );
 }
