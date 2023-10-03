02ea924b33554386b2a16955159fc4323da2e518
Andrew <andrew@andrewsmith.org>
2023-10-03 00:19:30 -0400

    Fit CharacterContainer on single page

--- app/_components/MissingItem.tsx
+++ app/_components/MissingItem.tsx
@@ -1,6 +1,6 @@
 export default function MissingItem() {
   return (
-    <div className="flex justify-center items-center bg-slate-800 h-20 m-2 rounded-md">
+    <div className="flex justify-center items-center bg-slate-800 h-16 m-2 rounded-md">
       <p className="italic text-slate-400">sparrow or ship not equipped</p>
     </div>
   );
