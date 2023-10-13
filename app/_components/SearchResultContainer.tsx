82849f72d27f630536421b21656f8033483665d3
Thomas Bigelow <70189857+TJBigelow@users.noreply.github.com>
2023-10-13 00:45:48 -0500

    I wrote this code on github

--- app/_components/SearchResultContainer.tsx
+++ app/_components/SearchResultContainer.tsx
@@ -20,12 +20,18 @@ export default function SearchResultContainer({
     setCurrentUserData({ membershipId, membershipType });
     setSearchResults([]);
   };
+  
+  const getPowerLevel = (acct) => {
+    const firstCharacterId = Object.keys(acct.characters.data)[0]
+    const firstCharacterObj = acct.characters.data[firstCharacterId]
+    const powerLevelKey = 1935470627
+    return firstCharacterObj.stats[powerLevelKey]
+  }
 
   // following sort operation is likely hard to follow due to the complicated shape of the data.  searchResult.characters.data is an object whose keys are strings containing characterIds.  We want to access data from the "first" characterId (as this is the information we're digging into in each SearchResult component).  Since that key is a different value in each searchResult, the most straightforward way to access it systematically is invoking Object.keys() here as well.  The stat we're sorting by at the end of the expression is the character's power level.
   searchResults.sort(
     (a, b) =>
-      b.characters.data[Object.keys(b.characters.data)[0]].stats[1935470627] -
-      a.characters.data[Object.keys(a.characters.data)[0]].stats[1935470627]
+      getPowerLevel(b) - getPowerLevel(a)
   );
 
   const searchResultComponents = searchResults.map((searchResult, i) => {
