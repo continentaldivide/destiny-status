b11331af1b784ce8e098b27c694d69dda5be8026
andrew <andrew@andrewsmith.org>
2023-10-22 01:42:29 -0400

    Change useManifestStatus output to string
    useManifestStatus had been returning a boolean ('is the manifest loaded?') but there are really three states I wanted to capture: performing the version check, downloading the manifest if needed, and confirmation.  This allows the app to be a little more expressive with the user in describing what's happening during loading.

--- app/_components/LoadingScreen.tsx
+++ app/_components/LoadingScreen.tsx
@@ -1,11 +1,14 @@
+import Image from "next/image";
+
 export default function LoadingScreen({
   loadingMessage,
 }: {
   loadingMessage: string;
 }) {
   return (
-    <div className="flex min-h-screen flex-col items-center pt-24">
+    <div className="min-h-screen flex gap-2 justify-center items-center">
       <p>{loadingMessage}</p>
+      <Image src="spinner.svg" alt="" width={24} height={24} className="animate-spin"/>
     </div>
   );
 }
