1cb707a1e1367e9115ad84a743ad73d464df0b4b
Andrew <andrew@andrewsmith.org>
2023-10-24 10:41:00 -0400

    Dim background; add rough style to modal

--- app/_components/InstructionsModal.tsx
+++ app/_components/InstructionsModal.tsx
@@ -7,11 +7,25 @@ export default function InstructionsModal({
 }) {
   return isOpen ? (
     <>
-      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 border border-pink-200 z-50">
-        <p>hello world</p>
-        <button onClick={handleClose} className="border border-gray-500">
-          close me
-        </button>
+      <div className="fixed inset-0 bg-neutral-900/40 z-50"></div>
+      <div className="fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-1/4 bg-blue-950  border-pink-200 rounded-md z-50">
+        <div className="flex flex-col gap-2 items-center m-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button
+            onClick={handleClose}
+            className="px-2 text-lg bg-blue-500 hover:bg-blue-600 border border-gray-500 rounded-md"
+          >
+            Got it!
+          </button>
+        </div>
       </div>
     </>
   ) : null;
