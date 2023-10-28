c3edd8fcd820c080371dc64b3e089fc03d16c55e
Andrew <andrew@andrewsmith.org>
2023-10-27 20:12:02 -0400

    Add title and gutters to nav

--- app/_components/Nav.tsx
+++ app/_components/Nav.tsx
@@ -15,31 +15,36 @@ export default function Nav({
   const [searchQuery, setSearchQuery] = useState('');
   return (
     <nav className="absolute w-full flex justify-center bg-slate-700 border-b border-slate-500">
-      <form action="" className="flex">
-        <input
-          value={searchQuery}
-          type="search"
-          placeholder="search by Bungie name..."
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="h-10 p-6 flex-grow text-2xl outline-none bg-slate-700"
-        />
+      <div className='flex relative justify-center w-3/4'>
+        <div className="absolute left-0 h-full pl-2 flex items-center">
+          <p className="text-2xl font-bold">Destiny Status</p>
+        </div>
+        <form action="" className="flex">
+          <input
+            value={searchQuery}
+            type="search"
+            placeholder="search by Bungie name..."
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="h-10 p-6 flex-grow text-2xl outline-none bg-slate-700"
+          />
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              setUsername(searchQuery);
+            }}
+            disabled={fetchingData}
+          >
+            <Image src="search.svg" alt="" width={36} height={36} />
+          </button>
+        </form>
         <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            setUsername(searchQuery);
-          }}
-          disabled={fetchingData}
+          onClick={handleInfoClick}
+          className="absolute right-0 h-full pr-2 hover:brightness-125"
         >
-          <Image src="search.svg" alt="" width={36} height={36} />
+          <img height="36" width="36" src="info.svg" />
         </button>
-      </form>
-      <button
-        onClick={handleInfoClick}
-        className="absolute right-0 h-full pr-2 hover:brightness-125"
-      >
-        <img height="36" width="36" src="info.svg" />
-      </button>
+      </div>
     </nav>
   );
 }
