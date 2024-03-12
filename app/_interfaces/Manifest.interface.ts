dd073bf309ed44f301b4bc82f964a6dc11295a36
Andrew <andrew@andrewsmith.org>
2024-03-12 01:01:18 -0400

    Add manifest table for perk lookup to browser manifest

--- app/_interfaces/Manifest.interface.ts
+++ app/_interfaces/Manifest.interface.ts
@@ -1,9 +1,11 @@
 import { DamageTableType } from './manifestTables/DestinyDamageTypeDefinition.interface';
 import { ItemTableType } from './manifestTables/DestinyInventoryItemDefinition.interface';
 import { StatTableType } from './manifestTables/DestinyStatDefinition.interface';
+import { PerkTableType } from './manifestTables/DestinySandboxPerkDefinition.interface';
 
 export default interface ManifestType {
   DestinyDamageTypeDefinition: DamageTableType;
   DestinyInventoryItemDefinition: ItemTableType;
+  DestinySandboxPerkDefinition: PerkTableType;
   DestinyStatDefinition: StatTableType;
 }
