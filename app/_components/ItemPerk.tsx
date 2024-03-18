dcfaa18e5b2956a51cb4bffbc6199f436347a7e2
Andrew <andrew@andrewsmith.org>
2024-03-18 18:19:10 -0400

    Add documentation to ItemPerk

--- app/_components/ItemPerk.tsx
+++ app/_components/ItemPerk.tsx
@@ -9,7 +9,9 @@ export default function ItemPerk({ itemPerk }: { itemPerk: ItemPerkType }) {
     return null;
   }
 
-  console.log(DestinySandboxPerkDefinition[itemPerk.perkHash]);
+  const perkDefinition = DestinySandboxPerkDefinition[itemPerk.perkHash];
+
+  // notes for future: (a) eventually, would like to create a tooltip on hovering the perk that provides the user with some of the information available via perkDefinition.  (b) it seems like some of the perk images provided by bungie's API (specifically most of the armor perks) are not fully transparent, leading to a noticeable background square around the outside of the actual icon.  tried getting rid of this a bunch of different ways and ultimately decided to ship for now and revisit later.  might be worth reaching out to a dev of a different app like DIM to try to learn whether other folks have run into this and how they've solved it.
 
   return (
     <Image
